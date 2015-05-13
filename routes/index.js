5a66a26a62e43f1b049817d9f86433b939877720
Guillaume.Grosshenny <guillaume.grosshenny@etu.unistra.fr>
2015-05-13 15:48:35 +0200

    c'est fini

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 var tweets = [];
+var utilisateurs = [];
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'The Lord of the Tweets', tweets: tweets });
+  res.render('index', { title: 'The Lord of the Tweets', tweets: tweets, utilisateurs: utilisateurs });
 });
 
 router.get('/inscription', function(req, res, next) {
@@ -19,6 +20,7 @@ router.get('/contact', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
     tweets.unshift(req.body.tweet);
+    utilisateurs.unshift(req.body.user);
     res.redirect('/')
 });
 
